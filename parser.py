b04960fd94dfed401b63e68a50a1642244890d71
Lucas Cherkewski <lucas@ecustom.ca>
2016-05-03 20:58:15 -0400

    awesome parser progress!

--- parser.py
+++ parser.py
@@ -1,16 +1,35 @@
 import os
 from bs4 import BeautifulSoup
 
-htmlFilesDir = 'pages/'
+htmlFilesDir = "pages/"
 htmlFiles = os.listdir(htmlFilesDir)
 
 files = []
 
+courses = []
+
 for file in htmlFiles:
-    if '.html' in file:
+    if ".html" in file:
         files.append(htmlFilesDir + file)
 
 for file in files:
-    soup = BeautifulSoup(open(file, 'r'), 'html.parser')
+    soup = BeautifulSoup(open(file, "r"), "html.parser")
 
-    print(soup.table)
+    courseTables = soup.find_all(name="table", id="crsBox")
+
+    for courseTable in courseTables:
+        courseData = {}
+
+        keysAndClasses = [
+            ["code", "crsCode"],
+            ["title", "crsTitle"],
+            ["description", "crsDesc"],
+            ["restriction", "crsRestrict"]
+        ]
+
+        for keyAndClass in keysAndClasses:
+            courseData[keyAndClass[0]] = courseTable.find(class_=keyAndClass[1])
+
+        courses.append(courseData.copy())
+
+print(courses)

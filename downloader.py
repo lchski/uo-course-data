db1ce711396d851ed716ff92c0b44ae87efa7c4d
Lucas Cherkewski <lucas@ecustom.ca>
2016-05-03 21:28:42 -0400

    rewrite downloader to output bytes data to file

--- downloader.py
+++ downloader.py
@@ -5,5 +5,5 @@ disciplines = ['HIS', 'POL']
 for discipline in disciplines:
     page = requests.get('http://www.uottawa.ca/academic/info/regist/calendars/courses/' + discipline + '.html')
 
-    file = open('pages/' + discipline + '.html', 'w')
-    file.write(page.text)
+    file = open('pages/' + discipline + '.html', 'wb')
+    file.write(page.content)

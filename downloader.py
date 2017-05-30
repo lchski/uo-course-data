e5f1b4c8dbbb4cef67b4250f93c8a14cb7ca9942
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-29 20:56:11 -0400

    update downloader path

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import requests
 disciplines = ['HIS', 'POL', 'PHI', 'LIN', 'FEM', 'FLS']
 
 for discipline in disciplines:
-    page = requests.get('http://www.uottawa.ca/academic/info/regist/calendars/courses/' + discipline + '.html')
+    page = requests.get('http://catalogue.uottawa.ca/en/courses/' + str.lower(discipline) + '/')
 
     file = open('pages/' + discipline + '.html', 'wb')
     file.write(page.content)
